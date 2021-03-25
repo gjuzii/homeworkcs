5f6e42e57ae8d98b39b91d9f4f872e8330ae31a8
Sara <76701761+gjuzii@users.noreply.github.com>
2021-03-25 17:25:21 +0100

    homework01 added

---
+++ homework/hm1/hm1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+        }
+    }
+}

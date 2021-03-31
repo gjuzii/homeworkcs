fefb65a90c7e3ecb5e914c665d87c6945eacc0c2
Sara <76701761+gjuzii@users.noreply.github.com>
2021-03-31 20:51:16 +0200

    bonus_homework

---
+++ bonus_homework/bonus_homework/homework01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the curent temperature:");
+            int temperature;
+
+            string input = Console.ReadLine();
+
+            bool checkInput = int.TryParse(input, out temperature);
+
+            if (checkInput)
+            {
+                Console.WriteLine("Must enter a number");
+            }
+            else if (temperature <= 0)
+            {
+                Console.WriteLine("Freezing weather.");
+            }
+            else if (temperature <= 10)
+            {
+                Console.WriteLine("Very cold weather.");
+            }
+            else if (temperature <= 20)
+            {
+                Console.WriteLine("Cold weather.");
+            }
+            else if (temperature <= 30)
+            {
+                Console.WriteLine("Normal in temp.");
+            }
+            else if (temperature >= 40)
+            {
+                Console.WriteLine("Its very hot.");
+            }
+
+            Console.WriteLine(checkInput);
+            Console.WriteLine(temperature);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bonus_homework/bonus_homework/homework02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Enter the first number");
+			int number1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter the second number");
+			int number2 = Convert.ToInt32(Console.ReadLine());
+			if (number1 == number2)
+			{
+				int sum = (number1 + number2) * 3;
+				Console.WriteLine("  {0} + {1} = {2}", number1, number2, sum);
+			}
+			else
+			{
+				int sum = (number1 + number2);
+				Console.WriteLine(" {0} + {1} = {2}", number1, number2, sum);
+			}
+
+			Console.ReadLine();
+		}
+    }
+}

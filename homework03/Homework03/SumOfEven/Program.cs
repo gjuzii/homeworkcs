45e32850e077712dc5508f56f99cab58fe83af2d
Sara <76701761+gjuzii@users.noreply.github.com>
2021-04-02 15:18:03 +0200

    Homework03

---
+++ homework03/Homework03/SumOfEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of Even");
+
+            int[] intArray = new int[6];
+            int sum = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+               Console.WriteLine("Enter a number: ");
+               intArray[i] = int.Parse(Console.ReadLine());             
+            }
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] % 2 == 0)
+                {
+                    sum = sum + intArray[i];
+                }
+                Console.WriteLine("The result of even numbers is:  " + sum);
+                
+            }
+            Console.ReadLine();                
+
+        }
+    }
+}
---
+++ homework03/Homework03/exercise01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Student Group");
+
+            string[] studentsG1 = new string[] { "Mila", "Ana", "Jana", "Igor", "Darko" };
+
+            string[] studentsG2 = new string[] { "Tea", "Aleksandar", "Daniel", "Ana", "Nela" };
+
+            Console.WriteLine("For group G1 enter 1 for G2 enter 2");
+
+            int inputNumber = int.Parse(Console.ReadLine());
+            if (inputNumber == 1)
+            {
+                Console.WriteLine("The students in G1 are: " + "\n" + studentsG1[0] + "\n" + studentsG1[1] + "\n" + studentsG1[2] + "\n" + studentsG1[3] + "\n" + studentsG1[4]);
+            }
+            else if (inputNumber == 2)
+            {
+                Console.WriteLine("The students in G2 are: "+ "\n" + studentsG2[0] + "\n" + studentsG2[1] + "\n" + studentsG2[2] + "\n" + studentsG2[3] + "\n" + studentsG2[4]);
+            }
+            else
+            {
+                Console.WriteLine("Please enter only numbers 1 or 2 .");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
